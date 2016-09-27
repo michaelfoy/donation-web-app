478cd92c67ab9a69f4f88ecca5a8f55a8116a0a5
Michael Foy <mikkfoy@gmail.com>
2016-09-27 12:05:37 +0100

    lab01 steps complete

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 'use strict';
 
 const Hapi = require('hapi');
-
 var server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 4000 });
+server.register(require('inert'), err => {
 
-server.start(err => {
   if (err) {
     throw err;
   }
 
-  console.log('Server listening at:', server.info.uri);
+  server.route(require('./routes'));
+  server.start(err => {
+    if (err) {
+      throw err;
+    }
+
+    console.log('Server listening at:', server.info.uri);
+  });
 });
